96317da113e84ddb4bc388d4718974f4b84f07f9
martin <mbogelund@gmail.com>
2023-12-03 15:19:10 +0100

    Finished part 1.

--- Dec03/Part_1.py
+++ Dec03/Part_1.py
@@ -28,6 +28,19 @@ def insert_symbol(conn, symbol_tuple):
     conn.commit()
     return cur.lastrowid
 
+def insert_number(conn, number_tuple):
+    """
+    Insert number
+    :param conn:
+    :param number:
+    :return: row id
+    """
+    sql = ''' INSERT INTO numbers(line_number, number, start_position, end_position)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, number_tuple)
+    conn.commit()
+    return cur.lastrowid
 
 # Import today's data
 data = [l.strip() for l in open("Input/input.txt", "rt")]
@@ -57,22 +70,38 @@ for line in data:
         position = line.find(symbol, position)
         symbol_data = (line_number, symbol, position, position + len(symbol) - 1)
         position += 1
-        print(symbol_data)
+        #print(symbol_data)
         row_id = insert_symbol(db, symbol_data)
     position = 0
     for number in numbers:
         position = line.find(number, position)
         number_data = (line_number, number, position, position + len(number) - 1)
         position += 1
-        print(number_data)
-    line_number += 1
+        #print(number_data)
+        row_id = insert_number(db, number_data)
     #print(line_number)
+    line_number += 1
     print(line)
-# Part 2
 
 
+db.execute("DROP TABLE IF EXISTS part_numbers")
+db.execute("CREATE TABLE part_numbers AS select distinct nbr.line_number, \
+                                                         sbl.line_number as symbol_line_number, \
+                                                         nbr.number as part_number, \
+                                                         sbl.symbol \
+FROM numbers AS nbr INNER JOIN symbols as sbl ON \
+nbr.line_number - 1 <= sbl.line_number AND nbr.line_number + 1 >= sbl.line_number \
+AND nbr.start_position - 1 <= sbl.end_position \
+AND nbr.end_position + 1 >= sbl.start_position")
+
+cur = db.execute("SELECT sum(part_number) from part_numbers")
+query_result = cur.fetchall()
+print(query_result)
 
 # Cleanup
 if db:
+    #db.execute("DROP TABLE IF EXISTS symbols")
+    #db.execute("DROP TABLE IF EXISTS numbers")
+    #db.execute("DROP TABLE IF EXISTS part_numbers")
     db.close()
 
