803eaf76f246790121e2a654b33be806ba610f60
martin <mbogelund@gmail.com>
2023-12-03 10:09:08 +0100

    Added initial database connectivity.

--- Dec03/Part_1.py
+++ Dec03/Part_1.py
@@ -1,6 +1,33 @@
 import re
+import sqlite3
+from sqlite3 import Error
 
-import db_tools
+# Database functions
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def insert_symbol(conn, symbol_tuple):
+    """
+    Insert symbol
+    :param conn:
+    :param symbol:
+    :return: row id
+    """
+    sql = ''' INSERT INTO symbols(line_number, symbol, start_position, end_position)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, symbol_tuple)
+    conn.commit()
+    return cur.lastrowid
+
 
 # Import today's data
 data = [l.strip() for l in open("Input/input.txt", "rt")]
@@ -10,7 +37,12 @@ data = [l.strip() for l in open("Input/input.txt", "rt")]
 # Part 1
 
 # Create needed database tables
-db.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, age NUMBER, fav_food STRING)")
+db = create_connection(r"data/pythonsqlite.db")
+db.execute("CREATE TABLE IF NOT EXISTS symbols (line_number NUMBER, symbol TEXT, start_position NUMBER, end_position NUMBER)")
+db.execute("DELETE FROM symbols")
+db.execute("CREATE TABLE IF NOT EXISTS numbers (line_number NUMBER, number STRING, start_position NUMBER, end_position NUMBER)")
+db.execute("DELETE FROM numbers")
+
 
 lines = []
 line_number = 0
@@ -26,6 +58,7 @@ for line in data:
         symbol_data = (line_number, symbol, position, position + len(symbol) - 1)
         position += 1
         print(symbol_data)
+        row_id = insert_symbol(db, symbol_data)
     position = 0
     for number in numbers:
         position = line.find(number, position)
@@ -36,4 +69,10 @@ for line in data:
     #print(line_number)
     print(line)
 # Part 2
+
+
+
+# Cleanup
+if db:
+    db.close()
 
