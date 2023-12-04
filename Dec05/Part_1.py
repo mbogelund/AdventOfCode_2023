7a46961548c504bd8375c2d0c5ea5fac9418111c
JB2253 Martin Bøgelund <jb2253@jyskebank.dk>
2023-12-04 23:57:28 +0100

    Added template for December 5.

---
+++ Dec05/Part_1.py
@@ -0,0 +1,111 @@
+# AoC 2023
+# Dec05
+
+import re
+import sqlite3
+from sqlite3 import Error
+
+# Database functions
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def insert_symbol(conn, symbol_tuple):
+    """
+    Insert symbol
+    :param conn:
+    :param symbol:
+    :return: row id
+    """
+    sql = ''' INSERT INTO symbols(line_number, symbol, start_position, end_position)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, symbol_tuple)
+    conn.commit()
+    return cur.lastrowid
+
+def insert_number(conn, number_tuple):
+    """
+    Insert number
+    :param conn:
+    :param number:
+    :return: row id
+    """
+    sql = ''' INSERT INTO numbers(line_number, number, start_position, end_position)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, number_tuple)
+    conn.commit()
+    return cur.lastrowid
+
+# Import today's data
+data = [l.strip() for l in open("Input/input.txt", "rt")]
+#print(data)
+
+
+# Part 1
+
+# Create needed database tables
+db = create_connection(r"data/pythonsqlite.db")
+db.execute("CREATE TABLE IF NOT EXISTS symbols (line_number NUMBER, symbol TEXT, start_position NUMBER, end_position NUMBER)")
+db.execute("DELETE FROM symbols")
+db.execute("CREATE TABLE IF NOT EXISTS numbers (line_number NUMBER, number STRING, start_position NUMBER, end_position NUMBER)")
+db.execute("DELETE FROM numbers")
+
+
+lines = []
+line_number = 0
+for line in data:
+    symbols = re.sub('\d', ' ', line).replace('.', ' ').split()
+    #print(symbols)
+    numbers =  re.sub('\D', ' ', line).replace('.', ' ').split()
+    #print(numbers)
+
+    position = 0
+    for symbol in symbols:
+        position = line.find(symbol, position)
+        symbol_data = (line_number, symbol, position, position + len(symbol) - 1)
+        position = position + len(symbol)
+        #print(symbol_data)
+        row_id = insert_symbol(db, symbol_data)
+    position = 0
+    for number in numbers:
+        position = line.find(number, position)
+        number_data = (line_number, number, position, position + len(number) - 1)
+        position = position + len(number)
+        #print(number_data)
+        row_id = insert_number(db, number_data)
+    #print(line_number)
+    line_number += 1
+    #print(line)
+
+
+db.execute("DROP TABLE IF EXISTS part_numbers")
+db.execute("CREATE TABLE part_numbers AS select distinct nbr.line_number, \
+                                                         sbl.line_number as symbol_line_number, \
+                                                         nbr.number as part_number, \
+                                                         sbl.symbol \
+FROM numbers AS nbr INNER JOIN symbols as sbl ON \
+nbr.line_number - 1 <= sbl.line_number AND nbr.line_number + 1 >= sbl.line_number \
+AND nbr.start_position - 1 <= sbl.end_position \
+AND nbr.end_position + 1 >= sbl.start_position")
+
+cur = db.execute("SELECT sum(part_number) from part_numbers")
+query_result = cur.fetchall()
+print(query_result)
+# Result: 554003
+
+# Cleanup
+if db:
+    #db.execute("DROP TABLE IF EXISTS symbols")
+    #db.execute("DROP TABLE IF EXISTS numbers")
+    #db.execute("DROP TABLE IF EXISTS part_numbers")
+    db.close()
+
---
+++ Dec05/Part_1.sas
@@ -0,0 +1,72 @@
+/* AoC 2023 */
+/* Dec05 */
+
+%put &=sysscp;
+%if %sysfunc(substr(&sysscp., 1, 3)) = WIN %then %do;
+  %let execPgmPath=%sysget(SAS_EXECFILEPATH); * Hvor ligger dette SAS-program der eksekveres lige nu? ;
+  %let pdlm=\;
+%end;
+%if %sysfunc(substr(&sysscp., 1, 3)) = LIN %then %do;
+  %let execPgmPath=&_SASPROGRAMFILE.; * Hvor ligger dette SAS-program der eksekveres lige nu? ;
+  %let pdlm=/;
+%end;
+%put &=pdlm;
+
+%let execPgm=%sysfunc(scan(&execPgmPath.,-1,&pdlm.));
+%let execPath=%sysfunc(substr(&execPgmPath., 1, %sysfunc(length(&execPgmPath.)) - %sysfunc(length(&execPgm.))));
+
+%put &=execPgmPath;
+%put &=execPgm;
+%put &=execPath;
+
+libname dat "&execPath.data";
+
+/* Import raw input */
+*filename in_file "&execPath.Input&pdlm.input.txt";
+filename in_file "&execPath.Input&pdlm.example.txt";
+data WORK.input;
+  length inline $ 1024;
+  infile in_file;
+  input;
+  inline = trim(left(_INFILE_));
+run;
+filename in_file clear;
+
+proc sql NOPRINT;
+  select max(length(inline)) into :max_length
+  from WORK.input
+  ;
+  select count(*) into :count_rows
+  from WORK.input
+  ;
+quit;
+%put &=max_length;
+%put &=count_rows;
+
+/* Part 1 */
+
+data _NULL_;
+  set WORK.input(obs=1);
+run;
+%put Some info...;
+%put some more info...;
+
+data WORK.scan;
+  set WORK.input;
+  /* ... */
+run;
+
+data WORK.PART1;
+  *set WORK.whatever(obs=1);
+  put 'Answer to part 1: ' part_1_answer;
+run;
+
+/* Result:  */
+/* Evaluation:  */
+
+
+/* Part 2? */
+
+
+/* Result:  */
+/* Evaluation:  */
